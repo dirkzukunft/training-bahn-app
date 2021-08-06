420f40069b084f56e3b02e3b74c0d67af334b10b
dirkzukunft <git@qf3.de>
2021-08-06 16:26:57 +0200

    Basis for stations auto suggestions

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,57 @@
 import './style.css';
-import { getStations } from '../utils/api-bahn';
+import { getStations, getTimetable } from '../utils/api-bahn';
 import { createElement } from '../utils/createElement';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
-//const stations = JSON.stringify(await getStations('leipzig'));
-//8010209
+const selectStation = createElement('section', {
+  childElements: [
+    createElement('label', {
+      innerText: 'Bahnhof:',
+      childElements: [
+        createElement('input', {
+          type: 'text',
+          id: 'search',
+          name: 'search',
+          oninput: loadStations,
+        }),
+      ],
+    }),
+    createElement('div', {
+      id: 'stations',
+    }),
+    createElement('div', {
+      id: 'result',
+    }),
+  ],
+});
+
+//console.log(await getTimetable('8010209'));
 
 if (app !== null) {
-  app.innerHTML = ``;
-  //app.append(createElement('p', { innerText: stations }));
+  app.append(selectStation);
+  document.querySelector('#search')?.setAttribute('list', 'stationsList');
 }
+
+async function loadStations() {
+  const searchElement = <HTMLInputElement>document.querySelector('#search');
+  const search = searchElement.value;
+  const stationsList = <HTMLElement>document.querySelector('#stations');
+
+  const stations = await getStations(search);
+
+  while (stationsList.firstChild) {
+    stationsList.removeChild(stationsList.firstChild);
+  }
+  stations.map((station) =>
+    stationsList.append(
+      createElement('a', {
+        innerText: station.name,
+        id: station.id,
+        class: 'station',
+      })
+    )
+  );
+}
+
+//   const result = <HTMLDivElement>document.querySelector('#result');
